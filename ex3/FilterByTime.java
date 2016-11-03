363417068bdab21c54b8d471cd4cffde0b8de3c5
wenrenqianmo <441185859@qq.com>
2016-11-03 16:55:08 +0800

    Update FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -1,38 +1,54 @@
-package No3;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+package no3;
+import java.io.FileInputStream; 
+import java.io.FileNotFoundException; 
+import java.text.ParseException; 
+import java.text.SimpleDateFormat; 
+import java.util.Date; 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern; 
 
-/*
- * 题目要求：
- * 0. 在个人仓库下，创建分支yourname_ex3
- * 1. 编写代码完成以下功能：
- * 		a. 从access.log中读入数据，获取IP和Time
- * 		b. 输出在时间区间[beginTime, endTime]内的IP和Time，以tab分割
- * 2. 提交代码到分支下，创建pull request，与主仓库的master分支对比
- */
+
 public class FilterByTime {
-	
-	public static void main(String[] args) throws ParseException, FileNotFoundException{
-		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
-		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
-		FileInputStream inputStream = new FileInputStream(filePath);
-		Scanner scanner = new Scanner(inputStream, "UTF-8");
-		while (scanner.hasNext()){
-			// 对每行进行处理
-			String line = scanner.nextLine();
-			// 切分获取IP，Time
-			String strIp = null;
-			String strTime = null;
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
-		}
-	}
-	
+	public static void main(String[] args) throws ParseException, FileNotFoundException{ 
+ 		//确定时间范围
+		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+ 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00"); 
+ 		Date endDate = regularFormat.parse("2015-12-31 19:00:00"); 
+ 		//文件获取
+ 		String filePath = "G:/eclipse/workspace/ex1/access.log"; 
+ 		FileInputStream inputStream = new FileInputStream(filePath); 
+ 		Scanner scanner = new Scanner(inputStream, "UTF-8"); 
+ 		//正则表达式
+ 		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]+ [^ ]+ \\[([^ ]+)";
+ 		Pattern r = Pattern.compile(pattern);
+ 		//提取时间转换格式
+ 		SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss");
+ 		
+ 		while (scanner.hasNext()){ 
+ 			// 对每行进行处理 
+ 			String line = scanner.nextLine(); 
+ 			// 切分获取IP，Time
+ 			Matcher m = r.matcher(line);
+ 			if(m.find()){
+ 				String strIp = m.group(1); 
+ 	 			String strTime = m.group(2);
+ 	 			//将String转换转换成时间
+ 	 			Date lineDate = null; 
+ 	 			lineDate = inputFormat.parse(strTime.replaceAll("Dec", "12"));
+ 	 			//将时间转换成毫秒数
+ 	 			long lineTimestamp;
+ 	 			long begin;
+ 	 			long end;
+ 	 			lineTimestamp = lineDate.getTime();
+ 	 			begin = beginDate.getTime();
+ 	 			end = endDate.getTime();
+ 	 			
+ 	 			// 比较时间，对在时间区间内的数据进行输出 
+ 	 			if((lineTimestamp>begin)&&(lineTimestamp<end)){
+ 	 				System.out.println(strIp + "\t" + strTime); 
+ 	 			}
+ 			}
+ 		} 
+ 	} 
 }
